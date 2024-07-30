37ab4fe2cdadaf31456aae64edd72a26e3b2b460
UP210676 <113133497+UP210676@users.noreply.github.com>
2024-07-29 23:03:25 -0600

    NavBar nuevo con iconos

--- frontend/src/components/Navbar/navbar.js
+++ frontend/src/components/Navbar/navbar.js
@@ -1,51 +1,67 @@
 import React from 'react';
+import { AppBar, Toolbar, Button, IconButton } from '@mui/material';
 import { Link } from 'react-router-dom';
-import { AppBar, Toolbar, Button } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import TaskIcon from '@mui/icons-material/Assignment';
+import ProjectIcon from '@mui/icons-material/Work';
+import LoginIcon from '@mui/icons-material/Login';
 
 const menuItems = [
-  {
-    id: 1,
-    name: 'Task',
-    path: '/task',
-  },
+
   {
     id: 2,
-    name: 'Project',
-    path: '/project',
+    name: 'Task',
+    path: '/task',
+    icon: <TaskIcon />,
   },
   {
     id: 3,
+    name: 'Project',
+    path: '/project',
+    icon: <ProjectIcon />,
+  },
+  {
+    id: 4,
     name: 'Login',
     path: '/login',
-  },
+    icon: <LoginIcon />,
+  }
 ];
 
-class Navbar extends React.Component {
-  renderMenuItems = (selectedItem) => {
-    this.setState({
-      actualPage: selectedItem
-    });
-  }
-
-  render() {
-    return (
-      <AppBar position="static">
-        <Toolbar>
-          {menuItems.map(item => (
-            <Button
-              key={item.id}
-              color="inherit"
-              component={Link}
-              to={item.path}
-              onClick={() => this.renderMenuItems(item.name)}
-            >
-              {item.name}
-            </Button>
-          ))}
-        </Toolbar>
-      </AppBar>
-    );
-  }
-}
+const Navbar = () => {
+  return (
+    <AppBar position="static" color="primary">
+      <Toolbar>
+        {menuItems.map((item) => (
+          <Button
+            key={item.id}
+            component={Link}
+            to={item.path}
+            startIcon={item.icon}
+            sx={{
+              color: 'white',
+              mx: 1.5,
+              px: 2,
+              py: 1,
+              borderRadius: 2,
+              backgroundColor: '#3f51b5',
+              '&:hover': {
+                backgroundColor: '#5c6bc0',
+                transform: 'scale(1.05)',
+                transition: 'transform 0.2s ease-in-out',
+              },
+              '&:active': {
+                transform: 'scale(0.95)',
+                transition: 'transform 0.1s ease-in-out',
+              },
+            }}
+          >
+            {item.name}
+          </Button>
+        ))}
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Navbar;

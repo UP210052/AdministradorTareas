dd1dc2101bfca5db67cb257296f0e99252237822
UP210052 <up210052@alumnos.upa.edu.mx>
2024-08-04 01:40:16 -0600

    Cambio de hostlocal a IP

--- backend/src/main/java/com/example/back/config/WebConfig.java
+++ backend/src/main/java/com/example/back/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://24.199.100.105/:3000")
+                        .allowedOrigins("http://24.199.100.105:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("Content-Type", "Access-Control-Allow-Headers", "Authorization", "X-Requested-With")
                         .allowCredentials(true);

e905cb95d5fd302098a7089ac2587064c287ca03
UP210052 <up210052@alumnos.upa.edu.mx>
2024-08-04 00:54:51 -0600

    Implementación de build

---
+++ backend/build/generated/sources/annotationProcessor/java/main/com/example/back/mapper/UserMapperImpl.java
@@ -0,0 +1,64 @@
+package com.example.back.mapper;
+
+import com.example.back.dto.UserDto;
+import com.example.back.models.UserModel;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-08-04T00:25:32-0600",
+    comments = "version: 1.4.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.7.jar, environment: Java 17.0.10 (Oracle Corporation)"
+)
+@Component
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public UserDto userToUserDto(UserModel userModel) {
+        if ( userModel == null ) {
+            return null;
+        }
+
+        UserDto userDto = new UserDto();
+
+        if ( userModel.getId() != null ) {
+            userDto.setId( userModel.getId().intValue() );
+        }
+        userDto.setNombre( userModel.getNombre() );
+        userDto.setEmail( userModel.getEmail() );
+        userDto.setPassword( userModel.getPassword() );
+
+        return userDto;
+    }
+
+    @Override
+    public UserModel userDtoToUserModel(UserDto userDto) {
+        if ( userDto == null ) {
+            return null;
+        }
+
+        UserModel userModel = new UserModel();
+
+        userModel.setNombre( userDto.getNombre() );
+        userModel.setEmail( userDto.getEmail() );
+        userModel.setPassword( userDto.getPassword() );
+        userModel.setId( (long) userDto.getId() );
+
+        return userModel;
+    }
+
+    @Override
+    public UserDto createUserDtoWithoutId(UserModel userModel) {
+        if ( userModel == null ) {
+            return null;
+        }
+
+        UserDto userDto = new UserDto();
+
+        userDto.setNombre( userModel.getNombre() );
+        userDto.setEmail( userModel.getEmail() );
+        userDto.setPassword( userModel.getPassword() );
+
+        return userDto;
+    }
+}
